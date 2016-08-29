6bf11462a2e1f8575a9c81b6b407a7d4c93be15a
Ben Clegg <bclegg@midcode.co.uk>
2016-08-29 22:21:30 +0100

    Added committee page. Needs populating.

--- public_html/committee.php
+++ public_html/committee.php
@@ -12,6 +12,171 @@
 			<?php include("header.html"); ?>
 			<div class="content">
 
+				<div class="row">
+					<div class="col-md-6">
+
+						<div class="committee-member">
+							<h3>Abby Smith</h3>
+							<h4>Secretary</h4>
+							<div class="left">
+								<img src="assets/logoSquare.png" alt="Sheffield Computer Science Society">
+							</div class="right">
+							<div>
+								<p>
+									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eros odio, dignissim vitae dignissim lobortis, lobortis at ex. Etiam neque nibh, pulvinar vitae urna eget, mattis dignissim sapien.
+								</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-6">
+
+						<div class="committee-member">
+							<h3>Andy Tyler</h3>
+							<h4>President</h4>
+							<div class="left">
+								<img src="assets/logoSquare.png" alt="Sheffield Computer Science Society">
+							</div class="right">
+							<div>
+								<p>
+									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eros odio, dignissim vitae dignissim lobortis, lobortis at ex. Etiam neque nibh, pulvinar vitae urna eget, mattis dignissim sapien.
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div class="row">
+					<div class="col-md-6">
+
+						<div class="committee-member">
+							<h3>Billy Dawson</h3>
+							<h4>Inclusions Officer</h4>
+							<div class="left">
+								<img src="assets/logoSquare.png" alt="Sheffield Computer Science Society">
+							</div class="right">
+							<div>
+								<p>
+									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eros odio, dignissim vitae dignissim lobortis, lobortis at ex. Etiam neque nibh, pulvinar vitae urna eget, mattis dignissim sapien.
+								</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-6">
+
+						<div class="committee-member">
+							<h3>Ben Clegg</h3>
+							<h4>Vice President</h4>
+							<div class="left">
+								<img src="assets/logoSquare.png" alt="Sheffield Computer Science Society">
+							</div class="right">
+							<div>
+								<p>
+									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eros odio, dignissim vitae dignissim lobortis, lobortis at ex. Etiam neque nibh, pulvinar vitae urna eget, mattis dignissim sapien.
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div class="row">
+					<div class="col-md-6">
+
+						<div class="committee-member">
+							<h3>James Webb</h3>
+							<h4>Social Secretary</h4>
+							<div class="left">
+								<img src="assets/logoSquare.png" alt="Sheffield Computer Science Society">
+							</div class="right">
+							<div>
+								<p>
+									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eros odio, dignissim vitae dignissim lobortis, lobortis at ex. Etiam neque nibh, pulvinar vitae urna eget, mattis dignissim sapien.
+								</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-6">
+
+						<div class="committee-member">
+							<h3>Joshua Case</h3>
+							<h4>Design</h4>
+							<div class="left">
+								<img src="assets/logoSquare.png" alt="Sheffield Computer Science Society">
+							</div class="right">
+							<div>
+								<p>
+									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eros odio, dignissim vitae dignissim lobortis, lobortis at ex. Etiam neque nibh, pulvinar vitae urna eget, mattis dignissim sapien.
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div class="row">
+					<div class="col-md-6">
+
+						<div class="committee-member">
+							<h3>Louis Thorpe-Monaghan</h3>
+							<h4>Design</h4>
+							<div class="left">
+								<img src="assets/logoSquare.png" alt="Sheffield Computer Science Society">
+							</div class="right">
+							<div>
+								<p>
+									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eros odio, dignissim vitae dignissim lobortis, lobortis at ex. Etiam neque nibh, pulvinar vitae urna eget, mattis dignissim sapien.
+								</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-6">
+
+						<div class="committee-member">
+							<h3>Martin Cnobel</h3>
+							<h4>Sponsorship</h4>
+							<div class="left">
+								<img src="assets/logoSquare.png" alt="Sheffield Computer Science Society">
+							</div class="right">
+							<div>
+								<p>
+									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eros odio, dignissim vitae dignissim lobortis, lobortis at ex. Etiam neque nibh, pulvinar vitae urna eget, mattis dignissim sapien.
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div class="row">
+					<div class="col-md-6">
+
+						<div class="committee-member">
+							<h3>Rob Ede</h3>
+							<h4>Technical Events Coordinator, Design</h4>
+							<div class="left">
+								<img src="assets/logoSquare.png" alt="Sheffield Computer Science Society">
+							</div class="right">
+							<div>
+								<p>
+									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eros odio, dignissim vitae dignissim lobortis, lobortis at ex. Etiam neque nibh, pulvinar vitae urna eget, mattis dignissim sapien.
+								</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-6">
+
+						<div class="committee-member">
+							<h3>Tom Borcherds</h3>
+							<h4>Treasurer</h4>
+							<div class="left">
+								<img src="assets/logoSquare.png" alt="Sheffield Computer Science Society">
+							</div class="right">
+							<div>
+								<p>
+									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eros odio, dignissim vitae dignissim lobortis, lobortis at ex. Etiam neque nibh, pulvinar vitae urna eget, mattis dignissim sapien.
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+
 			</div>
 		</div>
 	</body>
