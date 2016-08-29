aabdc685ac6c8b0292ff905d942943a88853c926
Ben Clegg <bclegg@midcode.co.uk>
2016-08-29 19:53:11 +0100

    Added navbar.

---
+++ public_html/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<?php include("meta.html"); ?>
+		<meta name="description" content="Sheffield Computer Science Society">
+		<title>Sheffield Computer Science Society</title>
+	</head>
+
+	<body>
+		<div class="container">
+			<?php include("header.html"); ?>
+			<div class="content">
+
+			</div>
+		</div>
+	</body>
+</html>
