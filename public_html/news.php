90650e1d279bdc5529006eb828068627fad567bd
Ben Clegg <bclegg@midcode.co.uk>
2016-08-29 18:53:26 +0100

    Imported old site.

---
+++ public_html/news.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<?php include("meta.html"); ?>
+		<meta name="description" content="Sheffield Computer Science Society">
+		<title>News - Sheffield Computer Science Society</title>
+	</head>
+
+	<body>
+		<div class="container">
+			<?php include("header.html"); ?>
+			<div class="content">
+	      <?php require "wp-blog-header.php";?>
+	      <?php
+	      $posts = get_posts('numberposts=20&order=DESC&orderby=date');
+	      foreach ($posts as $post) : start_wp();?>
+	      <?php echo '<article>';?>
+	      <?php echo '<h2>'; the_title(); echo '<br />'; echo '</h2>';?>
+	      <?php the_post_thumbnail(full);?>
+	      <?php the_content();?>
+	      <?php echo '</article>';?>
+	      <?php
+	      endforeach;?>
+			</div>
+		</div>
+	</body>
+</html>
