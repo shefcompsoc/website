553d4819a9fa1f17395b03659c07ae686987d66f
Rob Ede <robjtede@gmail.com>
2016-09-12 02:03:59 +0100

    php committee member insertion

--- public_html/committee.php
+++ public_html/committee.php
@@ -16,164 +16,27 @@
           <h1>Committee</h1>
         </div>
 
-        <div class="row">
+        <div class="committee-members">
 
-          <div class="col-md-6">
-
-            <div class="committee-member">
-              <h3>Abby Smith</h3>
-              <h4>Secretary</h4>
-              <div class="left">
-                <img src="assets/committee/abby.jpg" alt="Sheffield Computer Science Society">
-              </div>
+          <?php
+            include('./committee-info.php');
+            for ($i=0; $i<count($committee); $i++) {
+              $comem = $committee[$i];
+          ?>
+            <section class="committee-member">
+              <h3><?= $comem["name"] ?></h3>
+              <p class="role"><?= $comem["role"] ?></p>
               <div>
-                <p>
-                  I’m secretary meaning I keep these nerds in check.
-                  Don’t mean to brag but I got an A-level in IT so I know my way around a spreadsheet or two.
-                  I get pretty mental on the Comp Sci socials - one time at uBuffet I <i>almost</i> managed to eat 30 slices of cake as a bet… I ate 7.
-                </p>
+                <div class="avatar">
+                  <img src="assets/committee/<?= $comem["avatar"] ?>.jpg">
+                </div>
+                <p><?= $comem["bio"] ?></p>
               </div>
-              <div style="clear: both;"></div>
-            </div>
+            </section>
+          <?php } ?>
 
-            <div class="committee-member">
-              <h3>Andy Tyler</h3>
-              <h4>President</h4>
-              <div class="left">
-                <img src="assets/committee/andy.jpg" alt="Sheffield Computer Science Society">
-              </div>
-              <div>
-                <p>
-                  I’m the President which basically means I keep the society on track, and come up with fresh new ideas.
-                </p>
-              </div>
-              <div style="clear: both;"></div>
-            </div>
+      </div>
 
-            <div class="committee-member">
-              <h3>Billy Dawson</h3>
-              <h4>Inclusions Officer</h4>
-              <div class="left">
-                <img src="assets/committee/billy.jpg" alt="Sheffield Computer Science Society">
-              </div>
-              <div>
-                <p>
-                  You got a problem? Somebody getting you down? Don’t feel like you can come to an event?
-                  Come on down to your friendly neighbourhood Inclusions Officer and let’s talk it out.
-                  Savings, savings and more savings as I solve your problems and make events cater to your specific needs! Come see Billy today!
-                </p>
-              </div>
-              <div style="clear: both;"></div>
-            </div>
-
-            <div class="committee-member">
-              <h3>Ben Clegg</h3>
-              <h4>Vice President</h4>
-              <div class="left">
-                <img src="assets/committee/ben.jpg" alt="Sheffield Computer Science Society">
-              </div>
-              <div>
-                <p>
-                  My role is basically helping everyone do anything, and keeping things organised.
-                  Don't worry, I'm not planning on becoming a Frank Underwood.
-                  On a side note, do you have time to talk about our Lord and Saviour, Haxe?
-                </p>
-              </div>
-              <div style="clear: both;"></div>
-            </div>
-
-
-            <div class="committee-member">
-              <h3>James Webb</h3>
-              <h4>Social Secretary</h4>
-              <div class="left">
-                <img src="assets/committee/james.jpg" alt="Sheffield Computer Science Society">
-              </div>
-              <div>
-                <p>
-                  Social Secretary is just a fancy way of saying that my job is to help you have fun.
-                  Whether that be on a mental night out, cosy night in, or any other event organised
-                  by the society, I’ll be on hand to make sure we all have a great time. Let me know
-                  about any events you want to see organised or tickets you want sorting out.
-                </p>
-              </div>
-              <div style="clear: both;"></div>
-            </div>
-
-          </div> <!--end col-->
-          <div class="col-md-6">
-
-            <div class="committee-member">
-              <h3>Joshua Case</h3>
-              <h4>Media Team Member</h4>
-              <div class="left">
-                <img src="assets/committee/josh.jpg" alt="Sheffield Computer Science Society">
-              </div>
-              <div>
-                <p>
-                  I'm on the publicity team. This means that a third of the things you see on social media is from me. You know how you can talk to Billy about anything? Yeah, don't come to me. I'm known for being short and brown, and therefore I have a short temper. Have a nice day!
-                </p>
-              </div>
-              <div style="clear: both;"></div>
-            </div>
-
-            <div class="committee-member">
-              <h3>Louis Thorpe-Monaghan</h3>
-              <h4>Media Team Member</h4>
-              <div class="left">
-                <img src="assets/committee/louis.jpg" alt="Sheffield Computer Science Society">
-              </div>
-              <div>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eros odio, dignissim vitae dignissim lobortis, lobortis at ex. Etiam neque nibh, pulvinar vitae urna eget, mattis dignissim sapien.
-                </p>
-              </div>
-              <div style="clear: both;"></div>
-            </div>
-
-            <div class="committee-member">
-              <h3>Martin Cnobel</h3>
-              <h4>Sponsorship</h4>
-              <div class="left">
-                <img src="assets/committee/martin.jpg" alt="Sheffield Computer Science Society">
-              </div>
-              <div>
-                <p>
-                  As Company Interaction Officer, my job is to make us ~seem~ important so that companies will think we’re a big deal and give us money to.. Do stuff.. But they do offer job opportunities and internships, so it’s all good. I’m also pretty fun.
-                </p>
-              </div>
-              <div style="clear: both;"></div>
-            </div>
-
-            <div class="committee-member">
-              <h3>Rob Ede</h3>
-              <h4>Technical Events Co-ordinator, Media Team Member</h4>
-              <div class="left">
-                <img src="assets/committee/rob.jpg" alt="Sheffield Computer Science Society">
-              </div>
-              <div>
-                <p>
-                  <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" style="text-decoration: none;">The Technical Events Co-ordinatior co-ordinates technical events. Let's say there's an event and it's somewhat technical, chances are I was the guy who co-ordinated it. This includes but is not limited to Gamejams, Linux Tutorials and L33T H4CX0R1NG. Don't worry though, only good hacking. Promise.</a>
-                </p>
-              </div>
-              <div style="clear: both;"></div>
-            </div>
-
-            <div class="committee-member">
-              <h3>Tom Borcherds</h3>
-              <h4>Treasurer</h4>
-              <div class="left">
-                <img src="assets/committee/tom.jpg" alt="Sheffield Computer Science Society">
-              </div>
-              <div>
-                <p>
-                  hi im the treasurer, which is a p similar role to billys except the only problems i can help you with are ones related to neko atsume. ty for ur time
-                </p>
-              </div>
-              <div style="clear: both;"></div>
-            </div>
-
-        </div> <!-- end column -->
 
       </div>
     </div>
