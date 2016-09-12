73c18e798a86ef0f2a261aee8d5589c508e99c4e
Rob Ede <robjtede@gmail.com>
2016-09-12 02:18:40 +0100

    fix bios

--- public_html/committee-info.php
+++ public_html/committee-info.php
@@ -6,6 +6,11 @@
       'avatar' => 'andy',
       'bio' => 'I’m the President which basically means I keep the society on track...'
     ], [
+      'name' => 'Ben Clegg',
+      'role' => 'Vice President',
+      'avatar' => 'ben',
+      'bio' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    ], [
       'name' => 'Abby Smith',
       'role' => 'Secretary',
       'avatar' => 'abby',
@@ -29,29 +34,24 @@
       'bio' => 'hi im the treasurer, which is a p similar role to billys except the only problems i can help you with are ones related to neko atsume. ty for ur time'
     ], [
       'name' => 'Rob Ede',
-      'role' => 'Secretary',
-      'avatar' => 'abby',
+      'role' => 'Technical Events Co-ordinator, Media Team Member',
+      'avatar' => 'rob',
       'bio' => '<a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" style="text-decoration: none;">The Technical Events Co-ordinatior co-ordinates technical events. Let\'s say there\'s an event and it\'s somewhat technical, chances are I was the guy who co-ordinated it. This includes but is not limited to Gamejams, Linux Tutorials and L33T H4CX0R1NG. Don\'t worry though, only good hacking. Promise.</a>',
     ], [
       'name' => 'Joshua Case',
       'role' => 'Secretary',
-      'avatar' => 'abby',
+      'avatar' => 'josh',
       'bio' => 'I\'m on the publicity team. This means that a third of the things you see on social media is from me. You know how you can talk to Billy about anything? Yeah, don\'t come to me. I\'m known for being short and brown, and therefore I have a short temper. Have a nice day!'
     ], [
       'name' => 'Louis Thorpe-Monaghan',
-      'role' => 'Secretary',
-      'avatar' => 'abby',
+      'role' => 'Media Team Member',
+      'avatar' => 'louis',
       'bio' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
     ], [
       'name' => 'Martin Cnobel',
       'role' => 'Company Interactions Officer',
       'avatar' => 'martin',
       'bio' => 'As Company Interaction Officer, my job is to make us ~seem~ important so that companies will think we’re a big deal and give us money to.. Do stuff.. But they do offer job opportunities and internships, so it’s all good. I’m also pretty fun.'
-    ], [
-      'name' => 'Ben Clegg',
-      'role' => 'Vice President',
-      'avatar' => 'ben',
-      'bio' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
     ]
   ];
 ?>
