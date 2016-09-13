7ace52e481d9839edfe8bab82cc98ebdb39e195f
Rob Ede <robjtede@gmail.com>
2016-09-13 03:17:48 +0100

    add css purifier

--- gulpfile.js
+++ gulpfile.js
@@ -2,27 +2,40 @@ const gulp = require('gulp')
 const autoprefixer = require('gulp-autoprefixer')
 const livereload = require('gulp-livereload')
 const concat = require('gulp-concat')
+const cleancss = require('gulp-clean-css')
 const sass = require('gulp-sass')
+const purifycss = require('gulp-purifycss')
 const sourcemaps = require('gulp-sourcemaps')
+
+const paths = {
+  markup: ['./**/*.{html,php}'],
+  sass: ['./**/*.{sass,scss}'],
+  sassEntry: ['./public_html/css/style.scss'],
+  cssOut: './public_html/css'
+}
 
 // Build Tasks
 gulp.task('sass', () => {
-  return gulp.src(['./public_html/css/style.scss'])
+  return gulp.src(paths.sassEntry)
     .pipe(sourcemaps.init())
     .pipe(sass({
       outputStyle: 'compact'
     }).on('error', sass.logError))
     .pipe(concat('style.css'))
+    .pipe(purifycss(paths.markup))
     .pipe(autoprefixer({
       browsers: ['last 3 versions']
     }))
+    .pipe(cleancss({
+      keepBreaks: true
+    }))
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest('./public_html/css'))
+    .pipe(gulp.dest(paths.cssOut))
     .pipe(livereload())
 })
 
 gulp.task('markup', () => {
-  return gulp.src('./**/*.{html,php}')
+  return gulp.src(paths.markup)
     .pipe(livereload())
 })
 
@@ -31,8 +44,8 @@ gulp.task('markup', () => {
 gulp.task('observe', () => {
   livereload.listen()
 
-  gulp.watch('./**/*.{html,php}', ['markup'])
-  gulp.watch('./**/*.{scss,css}', ['sass'])
+  gulp.watch(paths.markup, ['markup'])
+  gulp.watch(paths.sass, ['sass'])
 })
 
 gulp.task('build', ['sass'])
