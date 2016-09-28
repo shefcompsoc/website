628aa326e9bfe5ee26a451cf6750f478896139a1
Rob Ede <robjtede@gmail.com>
2016-09-28 18:31:26 +0100

    add snapcode

---
+++ public_html/social.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+
+<html>
+  <head>
+    <?php include("meta.html"); ?>
+    <meta name="description" content="Sheffield Computer Science Society">
+    <title>Sheffield Computer Science Society</title>
+  </head>
+
+  <body>
+    <?php include("header.html"); ?>
+    <div class="container">
+      <div class="content">
+
+        <h1 class="title">Social</h1>
+
+        <section id="facebook" class="social">
+          <h1>Facebook</h1>
+        </section>
+
+        <section id="snapchat" class="social">
+          <h1>Snapchat</h1>
+          <p>
+            <a href="https://support.snapchat.com/en-GB/article/friends-snapcodes">Scan this with your phone</a> or <a href="https://support.snapchat.com/en-GB/a/add-snapcodes">screenshot it and add it in the Snapchat app.</a>
+          </p>
+          <img src="assets/snapcode.png" alt="" />
+        </section>
+
+      </div>
+    </div>
+    <?php include('footer.html'); ?>
+  </body>
+</html>
