abaf67c7fbcab3e5ba847fdc930b153af759ada2
Rob Ede <robjtede@gmail.com>
2016-09-28 18:44:22 +0100

    add facebook links

--- public_html/social.php
+++ public_html/social.php
@@ -16,6 +16,34 @@
 
         <section id="facebook" class="social">
           <h1>Facebook</h1>
+
+          <a href="https://www.facebook.com/shefcompsoc/">
+            <figure>
+              <img src="assets/fb_logo.png" alt="">
+              <figcaption>Main Facebook Page</figcaption>
+            </figure>
+          </a>
+          <br>
+          <a href="https://www.facebook.com/groups/1788205561451417/">
+            <figure>
+              <img src="assets/fb_logo.png" alt="">
+              <figcaption>CompSoc CS1 (First Year)</figcaption>
+            </figure>
+          </a>
+          <br>
+          <a href="https://www.facebook.com/groups/1092506430863758/">
+            <figure>
+              <img src="assets/fb_logo.png" alt="">
+              <figcaption>CompSoc CS2 (Second Year)</figcaption>
+            </figure>
+          </a>
+          <br>
+          <a href="https://www.facebook.com/groups/1101868363183656/">
+            <figure>
+              <img src="assets/fb_logo.png" alt="">
+              <figcaption>CompSoc CS3+ (Third Year or above)</figcaption>
+            </figure>
+          </a>
         </section>
 
         <section id="snapchat" class="social">
@@ -23,7 +51,7 @@
           <p>
             <a href="https://support.snapchat.com/en-GB/article/friends-snapcodes">Scan this with your phone</a> or <a href="https://support.snapchat.com/en-GB/a/add-snapcodes">screenshot it and add it in the Snapchat app.</a>
           </p>
-          <img src="assets/snapcode.png" alt="" />
+          <img src="assets/snapcode.png" alt="">
         </section>
 
       </div>
