468f56e111e677e0bc627e1c3d536f8bd0f0d5af
Rob Ede <robjtede@gmail.com>
2016-10-05 13:47:06 +0100

    shorten intro spiel

--- public_html/index.php
+++ public_html/index.php
@@ -11,23 +11,9 @@
     <?php include("header.html"); ?>
     <div class="container">
       <div class="content">
-        <h1 class="title">Welcome!</h1>
-
         <section class="intro">
           <p>Welcome to the University of Sheffield Computer Science Society!</p>
           <p>Every year we host a series of events with the aim of appealing to everyone interested in Computer Science at the University.</p>
-          <p>These are some of the events we plan to run this academic year:</p>
-          <ul>
-            <li>Annual Society Ball</li>
-            <li>Programming Competitions</li>
-            <li>Meals</li>
-            <li>Casual Meetups (for members to showcase their work)</li>
-            <li>Nights Out and Bar Crawls</li>
-            <li><a href="http://shefjam.shefcompsoc.co.uk" target="_blank">Game Jams</a> (weekend game development competitions)</li>
-            <li>Git Tutorials</li>
-            <li>GNU/Linux Tutorials</li>
-          </ul>
-          <p>Follow us on social media and <a href="http://signup.shefcompsoc.co.uk" target="_blank">signup to our mailing list</a> to receive event updates.</p>
         </section>
 
         <br>
