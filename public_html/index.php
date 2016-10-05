3ba5a2b8b542b2ed9eb7b08f00ed75a2952cfe0c
Rob Ede <robjtede@gmail.com>
2016-10-05 13:42:16 +0100

    add events section

--- public_html/index.php
+++ public_html/index.php
@@ -30,6 +30,52 @@
           <p>Follow us on social media and <a href="http://signup.shefcompsoc.co.uk" target="_blank">signup to our mailing list</a> to receive event updates.</p>
         </section>
 
+        <br>
+        <section class="intro">
+          <h2>Events</h2>
+          <div class="bubbles">
+            <figure>
+              <div class="icon"><img src="assets/gamejams.png" alt=""></div>
+              <figcaption>Game Jams</figcaption>
+            </figure>
+
+            <figure>
+              <div class="icon"><img src="assets/socials.png" alt=""></div>
+              <figcaption>Social Events</figcaption>
+            </figure>
+
+            <figure>
+              <div class="icon"><img src="assets/codetoast.png" alt=""></div>
+              <figcaption>CodeToast</figcaption>
+            </figure>
+
+            <figure>
+              <div class="icon"><img src="assets/tutorials.png" alt=""></div>
+              <figcaption>Tutorials</figcaption>
+            </figure>
+
+            <figure>
+              <div class="icon"><img src="assets/mentoring.png" alt=""></div>
+              <figcaption>Mentoring</figcaption>
+            </figure>
+
+            <figure>
+              <div class="icon"><img src="assets/summerball.png" alt=""></div>
+              <figcaption>Summer Ball</figcaption>
+            </figure>
+
+            <figure>
+              <div class="icon"><img src="assets/competitions.png" alt=""></div>
+              <figcaption>Coding Competitions</figcaption>
+            </figure>
+
+            <figure>
+              <div class="icon"><img src="assets/ctf.png" alt=""></div>
+              <figcaption>Capture The Flag</figcaption>
+            </figure>
+          </div>
+        </section>
+
 <!--
         <div class="page-title">
           <h1>Our Sponsors</h1>
