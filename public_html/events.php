e3289bf4e145b8e3fcd278273327ba777d559463
Rob Ede <robjtede@gmail.com>
2016-10-05 13:45:22 +0100

    add events page

---
+++ public_html/events.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+
+<html>
+  <head>
+    <?php include("meta.html"); ?>
+    <meta name="description" content="Sheffield Computer Science Society">
+    <title>Sheffield Computer Science Society</title>
+  </head>
+
+  <body>
+    <?php include("header.html"); ?>
+    <div class="container">
+      <div class="content">
+
+        <h1 class="title">Committee</h1>
+
+
+
+      </div>
+    </div>
+    <?php include('footer.html'); ?>
+  </body>
+</html>
