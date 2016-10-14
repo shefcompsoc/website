ac69b22af40e516c85337f96f825cc79ad693f40
Rob Ede <robjtede@gmail.com>
2016-10-14 17:22:27 +0100

    merge membership and signup

---
+++ routes/signup.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const Debug = require('debug')
+const debug = new Debug('app:routes/membership.js')
+
+module.exports.init = router => {
+  router.get('/signup', index)
+  router.get('/register', register)
+  router.get('/membership', membership)
+}
+
+const index = function* () {
+  debug('rendering signup page')
+  yield this.render('signup')
+}
+
+const register = function* () {
+  debug('redirecting to signup google form')
+  yield this.redirect('http://signup.shefcompsoc.co.uk')
+}
+
+const membership = function* () {
+  debug('redirecting to SU membership')
+  yield this.redirect('http://su.sheffield.ac.uk/groups/computer-science--2')
+}
