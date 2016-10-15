d2162f7961e934c6ae352606d5078914e001b5d9
Rob Ede <robjtede@gmail.com>
2016-10-15 17:16:09 +0100

    add shefjam page

--- routes/events.js
+++ routes/events.js
@@ -5,9 +5,21 @@ const debug = new Debug('app:routes/events.js')
 
 module.exports.init = router => {
   router.get('/events', index)
+  router.get('/event', events)
+  router.get('/event/shefjam', shefjam)
+}
+
+const events = function* () {
+  debug('redirecting to events page')
+  this.redirect('/events')
 }
 
 const index = function* () {
   debug('rendering events page')
   yield this.render('events')
+}
+
+const shefjam = function* () {
+  debug('rendering shefjam page')
+  yield this.render('pages/shefjam')
 }
