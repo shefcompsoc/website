0387bd741af5106b01ffae9552309f3da4d1ba94
Rob Ede <robjtede@gmail.com>
2016-10-25 02:43:42 +0100

    improve nav hover

--- src/scripts/fixheader.js
+++ src/scripts/fixheader.js
@@ -3,3 +3,20 @@ document.addEventListener('DOMContentLoaded', function () {
     Stickyfill.add(el)
   })
 })
+
+document.addEventListener('DOMContentLoaded', function () {
+  var nav = document.querySelector('nav')
+  Array.prototype.slice.call(nav.querySelectorAll('a')).forEach(function (el) {
+    var pos = 0
+
+    function animate (ev) {
+      pos += 100
+      this.style.setProperty('background-position', '0 ' + pos + '%')
+    }
+
+    el.addEventListener('mouseenter', animate)
+    el.addEventListener('mouseleave', animate)
+    el.addEventListener('focus', animate)
+    el.addEventListener('blur', animate)
+  })
+})
