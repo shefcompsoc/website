c5d5054f74b641899ee705464f083023660da44e
Brad Sharp <brad.z.sharp@gmail.com>
2017-02-20 23:53:23 +0000

    Complete version
    Be aware this won't work if the VMs time is incorrect

--- routes/challenge.js
+++ routes/challenge.js
@@ -3,6 +3,13 @@
 const Debug = require('debug')
 const debug = new Debug('app:routes/challenge.js')
 
+const deadline = new Date(2017, 1, 22, 16, 19, 40)
+
+function isActive() {
+  var t = Date.parse(deadline) - Date.parse(new Date());
+  return t <= 0
+}
+
 module.exports.init = router => {
   router.get('/challenges', index)
   router.get('/challenge', redir)
@@ -11,9 +18,15 @@ module.exports.init = router => {
 
 const index = async ctx => {
   debug('rendering challenges page')
-  await ctx.render('new/challenge/challenges', {
-    challenges
-  })
+  var page = {
+    active: isActive(),
+    challenges: challenges
+  }
+  if (true) {
+    await ctx.render('new/challenge/challenges', {
+      page
+    })
+  }
 }
 
 const redir = async ctx => {
@@ -23,20 +36,34 @@ const redir = async ctx => {
 
 const details = async ctx => {
   debug('rendering challenge details page')
-  var challenge = challenges[ctx.params.id]
-  if (challenge == null) {
-    ctx.redirect('challenges')
-  } else {
+  var challenge = challenges[ctx.params.id.toLowerCase()]
+  if (challenge != null && isActive()) {
     await ctx.render('new/challenge/details', {
       challenge
     })
+  } else {
+    ctx.redirect('../challenges')
   }
+
+}
+
+const error = {
+    name: 'Error',
+    tasks: [
+      'That seems to be an error',
+      'Go back using the button above'
+    ],
+    difficulty: 0
 }
 
 const challenges = {
-  Piprime: {
-    name: 'Pi prime',
-    task: '',
+  mipiprime: {
+    name: 'MiPi prime',
+    tasks: [
+      'Ever played Fizzbuzz? The rules to this are quite similar.',
+      'According to Wikipedia, FizzBuzz is a group word game for children to teach them about division. This may or may not be true, but this question is generally used to torture screen young computer science graduates during programming interviews.',
+      'Your task is to write a program that will play the game up to 1000, however there\'s a twist. For any number that is prime, you must instead display "Prime". For any number that is a factor of 3140 you must display "MiPi". If the number fits in both of these you must display "Piprime". All other numbers should be displayed as normal.'
+    ],
     difficulty: 3
   }
 }
