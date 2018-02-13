bd4c1f670224fe675070c06c1a39673d42f66914
Rob Ede <robjtede@icloud.com>
2018-02-13 14:49:38 +0000

    fix typo

--- eventdb.js
+++ eventdb.js
@@ -76,7 +76,7 @@ const process = db => {
       }
 
       const full = {
-        1: 'January', 2: 'Febrary', 3: 'March', 4: 'April', 5: 'May', 6: 'June',
+        1: 'January', 2: 'February', 3: 'March', 4: 'April', 5: 'May', 6: 'June',
         7: 'July', 8: 'August', 9: 'September', 10: 'October', 11: 'November', 12: 'December'
       }
 
